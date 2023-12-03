ade9ff3928dc694c25c826cb4750b1bfadc8c8d7
yona.moreda <yona.moreda@rabobank.nl>
2023-12-03 17:02:40 +0100

    Implement splash page - added earth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import earth from './img/8k-earth-nightmap.jpg'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <>
+      <img src={earth}/>
+      </>
+    {/*<App />*/}
   </React.StrictMode>,
 )
