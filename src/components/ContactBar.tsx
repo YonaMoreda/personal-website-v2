5084785b7b1ffa17acd16e2a1464131a34158744
yonamoreda <yonamoreda@gmail.com>
2025-02-02 13:53:50 +0100

    uploaded resume_yona_moreda.pdf refactored ScrollToRef.tsx

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -8,10 +8,8 @@ function ContactBar() {
     return (
         <div className="ContactBar">
             <a href={"https://github.com/YonaMoreda/"}><img style={{borderRadius: "50%"}} src={githubLogo}/></a>
+            <a href={ResumeFile} target={'_blank'}><img style={{width: '40px', padding: '0', borderRadius: "10%"}} src={ResumeIcon} alt={'resume'}/></a>
             <a href={"https://nl.linkedin.com/in/yona-moreda-116154141"}><img src={linkedinLogo} alt={'linkedin'}/></a>
-            <a href={ResumeFile} target={'_blank'}>
-                <img style={{width: '40px', padding: '0'}} src={ResumeIcon} alt={'resume'}/>
-            </a>
         </div>
     )
 }
